74e149ac6ca33275f157435b9ba9bef81aea040b
Diego Costantino <diegocstn@gmail.com>
2017-01-20 00:04:57 +0100

    fix ex1.3

--- chapter1/exercises/ex1.3.die.scm
+++ chapter1/exercises/ex1.3.die.scm
@@ -2,10 +2,12 @@
 (define (sum-of-square x y) (+ (square x) (square y)))
 (define (>= n1 n2)
   (if (> (- n1 n2) -1)
-    n1
-    n2
+    #t
+    #f
   )
 )
 (define (maxsquare n1 n2 n3)
-  (sum-of-square (>= n1 n2) (>= n2 n3))
-)
+  (cond ( (and (>= n1 n2) (>= n2 n3)) (sum-of-square n1 n2))
+	( (and (>= n2 n1) (>= n3 n1)) (sum-of-square n2 n3))
+	( (and (>= n1 n2) (>= n3 n2)) (sum-of-square n1 n3)))
+ )
