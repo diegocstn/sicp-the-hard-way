fd7feed5a31f0df0894ffc5e82b1df517d6f0938
Samuel Brandão <gb.samuel@gmail.com>
2017-02-28 11:52:48 +0100

    Rename exercise files

--- chapter1/exercises/ex1.10.sam.scm
+++ chapter1/exercises/ex1.10.sam.scm
@@ -50,7 +50,3 @@
 (define (f n) (A 0 n)) ; defines 2 * n
 (define (g n) (A 1 n)) ; defines 2 ^ n
 (define (h n) (A 2 n)) ; 2^2^2^...^2^2 (n times)
-
-; Exercise 1.11
-; f(n) = n if n < 3
-; f(n) = f(n−1) + 2f(n−2) + 3f(n−3) if n ≥ 3
